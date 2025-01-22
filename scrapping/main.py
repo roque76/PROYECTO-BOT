362ac4bba28cf9748fdb19f321dbbd505dd0564d
roque76 <115102140+roque76@users.noreply.github.com>
2025-01-21 20:04:35 -0500

    Time problem found

--- scrapping/main.py
+++ scrapping/main.py
@@ -1,13 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
 import re
+import time
 
 #Create headers to simulate a real request, parse the requested HTML
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'}
 url = 'https://bitinfocharts.com/bitcoin/address/1Ay8vMC7R1UbyCCZRVULMV7iQpHSAbguJP'
-r = requests.get(url, headers=headers)
-print(r.status_code)
+try:
+    start_time = time.time()
+    r = requests.get(url, headers=headers)
+    print(r.status_code)
+    print(f"Request time: {time.time() - start_time:.2f} seconds")
+except requests.exceptions.Timeout:
+    print("Timed out")
+
 soup = BeautifulSoup(r.content, 'html.parser')
 #Find the data needed
 table = soup.find('table', id='table_maina')
