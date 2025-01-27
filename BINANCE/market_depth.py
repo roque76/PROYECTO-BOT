af0eb1f4fd9e62208b7eeadd943ff2c095d66f7e
roque76 <115102140+roque76@users.noreply.github.com>
2025-01-27 14:35:01 -0500

    Market depth calculated

--- BINANCE/market_depth.py
+++ BINANCE/market_depth.py
@@ -5,9 +5,6 @@ from decimal import Decimal
 
 class WebSocket():
      def __init__(self):
-          self.avg_price = 0
-          self.bids = {}
-          self.asks = {}
           self.ws_order_book = SpotWebsocketAPIClient(on_open=self.on_open,on_close=self.on_close,
                                                        on_error=self.on_error, on_message=self.order_book)
           self.ws_average_price = SpotWebsocketAPIClient(on_open=self.on_open, on_close=self.on_close
@@ -22,6 +19,8 @@ class WebSocket():
           print(f'Error: {error}')
 
      def order_book(self, ws, message):
+          self.bids = {}
+          self.asks = {}
           message = json.loads(message)
           bids = message["result"]["bids"]
           asks = message["result"]["asks"]
@@ -29,22 +28,45 @@ class WebSocket():
           for price, quantity in bids:
                self.bids[float(price)] = float(quantity)
           for price, quantity in asks:
-               self.asks[float  (price)] = float(quantity)
-     
+               self.asks[float(price)] = float(quantity)
+
      def average_price(self, ws, message):
           message =  json.loads(message)
-          print(message["result"]["price"])
-          self.avg_price = message["result"]["price"]
+          self.avg_price = float(message["result"]["price"])
+          
 
-     
+     def market_depth(self, percentage=1):
+          percentage = (self.avg_price*percentage)/100
+          lowerLimit = self.avg_price-percentage
+          upperLimit = self.avg_price+percentage
+          
+          bid_depth = 0
+          ask_depth = 0
+          
+          print(f'Value now bids: {bid_depth}')
+          print(f'Value now asks: {ask_depth}')
 
+
+          for price in self.bids:
+               if lowerLimit <= price and upperLimit >= price:
+                    bid_depth+=self.bids[price]
+
+          for price in self.asks:
+               if lowerLimit <= price and upperLimit >= price:
+                    ask_depth+= self.asks[price]
+
+          print(f'Average price {self.avg_price}')
+          print(f'Bid depth {bid_depth}')     
+          print(f'Ask depth {ask_depth}')     
+               
 
      def start_client(self):
           while True:     
                try:
-                    self.ws_order_book.order_book(symbol='btcusdt', limit= 5)
+                    self.ws_order_book.order_book(symbol='btcusdt', limit= 100)
                     self.ws_average_price.avg_price(symbol='btcusdt')
                     time.sleep(5)
+                    self.market_depth()
                except KeyboardInterrupt:
                     self.ws_average_price.stop()
                     self.ws_order_book.stop()
